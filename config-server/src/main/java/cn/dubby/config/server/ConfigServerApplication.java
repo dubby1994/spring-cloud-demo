59a8212ab86c8f57200b0e703b55effa0b914366
dubby <yangzheng03@meituan.com>
2017-09-21 17:10:39 +0800

    add config server

---
+++ config-server/src/main/java/cn/dubby/config/server/ConfigServerApplication.java
@@ -0,0 +1,16 @@
+package cn.dubby.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created by teeyoung on 17/9/21.
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
