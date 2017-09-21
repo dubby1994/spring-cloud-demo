20d001ed16b9160f7c3ec5f653bc91dca74ab45e
dubby <yangzheng03@meituan.com>
2017-09-21 15:49:20 +0800

    add eureka

---
+++ eureka/src/main/java/cn/dubby/eureka/EurekaApplication.java
@@ -0,0 +1,19 @@
+package cn.dubby.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by teeyoung on 17/9/21.
+ * 服务发现
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
